d58575f2ccfa44e4b7157e688fb217b608c19e35
Tener <gtener+github@gmail.com>
2010-02-18 00:02:50 +0100

    Change in options: source locations and destination location are now passed as simple arguments.

--- src/Options.hs
+++ src/Options.hs
@@ -6,35 +6,34 @@ import System.Console.GetOpt
 import Data.Accessor
 import Data.Accessor.Tuple
 import Data.Accessor.Template
+import Text.Printf
 
 data CopyMode = Move | Copy
                 deriving (Read,Show,Eq,Ord)
 
 data RunOptions = RunOptions { 
-      sourceDir_   :: FilePath     -- ^ take files from this directory
-    , destDir_     :: FilePath     -- ^ put files in this directory
-    , filePattern_ :: String       -- ^ output file pattern
+      filePattern_ :: String       -- ^ output file pattern
     , copyMode_    :: CopyMode     -- ^ should we move or copy files?
-    }
+    } deriving (Read,Show,Eq,Ord)
 
+                         
 data EnvOptions = EnvOptions {
       showHelp_ :: Bool -- ^ show help
     , showVers_ :: Bool -- ^ show version on run
+    , showDefs_ :: Bool -- ^ show program defaults
     , continue_ :: Bool -- ^ set to False if either showVers or showHelp is True. if True program will not be run.
 
 --    NOT IMPLEMENTED YET:
     , logger_   :: String
     , verbose_  :: Bool
-    }
+    } deriving (Read,Show,Eq,Ord)
 
 class Default a where
     def :: a
 
 instance Default RunOptions where
     def = RunOptions {
-            sourceDir_   = "."
-          , destDir_     = "."
-          , filePattern_ = "{basename}-{date %Y-%m-%d}.{ext}"
+            filePattern_ = "{basename}-{date %Y-%m-%d}.{ext}"
           , copyMode_    = Copy
           }
 
@@ -42,41 +41,47 @@ instance Default EnvOptions where
     def = EnvOptions {
             showHelp_ = False
           , showVers_ = False
+          , showDefs_ = False
           , continue_ = True
           , logger_   = ""
           , verbose_  = False
           }
 
 type Options = (EnvOptions,RunOptions)
+type ErrorString = String
 
 $( deriveAccessors ''RunOptions )
 $( deriveAccessors ''EnvOptions )
 
 options :: [OptDescr (Options -> Options)]
-options = [ Option ['h','?'] ["help"] (NoArg ((first .> showHelp) ^= True)) "show help"
-          , Option ['V'] ["version"] (NoArg ((first .> showVers) ^= True)) "show version"
+options = [ Option ['h','?'] ["help"] (NoArg ((first .> continue ^= False) . 
+                                              (first .> showHelp ^= True)))
+                   "show help"
+          , Option ['V'] ["version"] (NoArg ((first .> continue ^= False) .
+                                              (first .> showVers ^= True)))
+                   "show version"
+          , Option [] ["show-defaults"] (NoArg ((first .> continue ^= False) .
+                                                (first .> showDefs ^= True)))
+                   "show program defaults"
+          , Option ['p'] ["pattern"] (ReqArg (second .> filePattern ^=) "PATTERN")
+                   "pattern for final files"
+          , Option ['m'] ["move"] (NoArg (second .> copyMode ^= Move))
+                   "set copy mode to 'move'"
+          , Option ['c'] ["copy"] (NoArg (second .> copyMode ^= Copy))
+                   "set copy mode to 'copy'"
+--          TODO: logger, verbose          
           ]
 
+usage = usageInfo header options where
+    header = "Usage: minirotate [OPTIONS] LOCATION [LOCATION...] DESTINATION"
+                                     
 
-{-
-options :: [OptDescr (Options -> Options)]
-options =
-     [ Option ['v']     ["verbose"]
-        (NoArg (\ opts -> opts { optVerbose = True }))
-        "chatty output on stderr"
-     , Option ['V','?'] ["version"]
-        (NoArg (\ opts -> opts { optShowVersion = True }))
-        "show version number"
-     , Option ['o']     ["output"]
-        (OptArg ((\ f opts -> opts { optOutput = Just f }) . fromMaybe "output")
-        "FILE")
-        "output FILE"
-     , Option ['c']     []
-        (OptArg ((\ f opts -> opts { optInput = Just f }) . fromMaybe "input")
-        "FILE")
-        "input FILE"
-     , Option ['L']     ["libdir"]
-        (ReqArg (\ d opts -> opts { optLibDirs = optLibDirs opts ++ [d] }) "DIR")
-        "library directory"
-     ]
--}
+parseOptions :: [String] -> Either ErrorString ([FilePath],Options)
+parseOptions args = case getOpt Permute options args of
+                               (o,places,[]) -> Right $ (places, foldl (flip id) (def,def) o)
+                               (_,_,err) -> 
+                                 let errs = if not (null err) 
+                                             then "Bad options: \n" ++ concat err ++ "\n" 
+                                             else ""
+                                 in
+                                   Left $ errs ++ usage
